f1101940e6089f81b7a418ca12f63905de2c396e
Leon Stam <leon@melonats.dev>
2025-03-26 11:03:43 +0100

    Add Horizon & Sentry + Add user types to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
+        'is_puzzle_user',
     ];
 
     /**
@@ -43,6 +45,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
+            'is_puzzle_user' => 'boolean',
         ];
     }
 }
